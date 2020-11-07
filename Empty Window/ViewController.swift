edc96e964fc03b0d5fcc47bbe6e78023cc2ea637
Saj Panchal <sajpanchal@gmail.com>
2020-11-07 18:35:03 -0500

    Initial Commit

---
+++ Empty Window/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Empty Window
+//
+//  Created by saj panchal on 2020-11-07.
+//  Copyright © 2020 saj panchal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
